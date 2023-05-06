52590d39031e48a5a95e89455f553fd630bcffce
Sam Mear <sam.mear2@gmail.com>
2023-05-06 18:39:58 +0100

    pc crashing, commiting changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ import {
   Code,
   Grid,
   theme,
-  Card, CardHeader, CardBody, CardFooter 
+  Card, CardHeader, CardBody, CardFooter, Container, Heading 
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
@@ -16,31 +16,11 @@ import { Logo } from './Logo';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Card>
-              <CardBody>
-                <Text>woo text!</Text>
-              </CardBody>
-            </Card>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Container>
+        <ColorModeSwitcher></ColorModeSwitcher>
+        <Heading>SamTFLApp</Heading>
+        <Text>An app that uses the TFL API.</Text>
+      </Container>
     </ChakraProvider>
   );
 }
