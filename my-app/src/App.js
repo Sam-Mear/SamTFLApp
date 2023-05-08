484bf52f7f8226def1e8e93bc4fff5ad523e9666
Sam Mear <sam.mear2@gmail.com>
2023-05-08 17:42:43 +0100

    Fixed bug on maps
    Additional info wont show sometimes.This has been fixed.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -31,6 +31,13 @@ const options = {
 function App() {
   const [data, setData] = useState([]);
   const [accidentData, setAccidentData] = useState([]);
+
+  // Following are variables that makes sure the maps arent drawn until the data is available
+  // Without this sometimes maps would load with no additional markers or heat maps on them.
+  const [showHeatmap, setShowHeatmap] = useState(false);
+  const [showLocationmap, setShowLocationmap] = useState(false);
+
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
@@ -43,11 +50,13 @@ function App() {
       const response = await fetch('https://api.tfl.gov.uk/BikePoint/');
       const data = await response.json();
       setData(data);
+      setShowLocationmap(true);//set the state to true once the data has been fetched
     }
     async function getAccidents() {
       const response2 = await fetch('https://api.tfl.gov.uk/AccidentStats/2019');
       const accidentData = await response2.json();
       setAccidentData(accidentData);
+      setShowHeatmap(true); //set the state to true once the data has been fetched
     }
     getData();
     getAccidents();
@@ -70,59 +79,67 @@ function App() {
         <ColorModeSwitcher />
         <Heading>SamTFLApp</Heading>
         <Text>An app that uses the TFL API.</Text>
-        <Heading as='h4' size='md'>Bike locations</Heading>
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          zoom={10}
-          center={center}
-          options={options}
-        >
-          {data.map((datapoint) => (
-            <Marker
-              key={datapoint.id}
-              position={{
-                lat: datapoint.lat,
-                lng: datapoint.lon,
-              }}
-              onClick={() => {
-                setSelected(datapoint);
-              }}
-            />
-          ))}
-
-          {/*Render an InfoBox component when a Marker is selected*/}
-          {selected && (
-            <InfoBox
-              position={{
-                lat: selected.lat,
-                lng: selected.lon,
-              }}
-              onCloseClick={() => {
-                setSelected(null);
-              }}
+        {showLocationmap && (
+          <Container>
+            <Heading as='h4' size='md'>Bike locations</Heading>
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              zoom={10}
+              center={center}
+              options={options}
             >
-              <div style={{ backgroundColor: "black" }}>
-                <Heading as="h2" size="md">
-                  {selected.commonName}
-                </Heading>
-                <Text>{selected.id}</Text>
-                <Text>{selected.additionalProperties.find(prop => prop.key === 'NbBikes').value} bike(s) available</Text>
-                <Text>Last updated: {selected.additionalProperties.find(prop => prop.key ==='NbBikes').modified}</Text>
-              </div>
-            </InfoBox>
-          )}
-        </GoogleMap>
-        <Heading as='h4' size='md'>Bike accident heat map (2019 data)</Heading>
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          zoom={10}
-          center={center}
-          options={options}
-        >
-          <HeatmapLayer 
-            data={pedalCycleAccidentCoords}
-          />
-        </GoogleMap>
+              {data.map((datapoint) => (
+                <Marker
+                  key={datapoint.id}
+                  position={{
+                    lat: datapoint.lat,
+                    lng: datapoint.lon,
+                  }}
+                  onClick={() => {
+                    setSelected(datapoint);
+                  }}
+                />
+              ))}
+
+              {/*Render an InfoBox component when a Marker is selected*/}
+              {selected && (
+                <InfoBox
+                  position={{
+                    lat: selected.lat,
+                    lng: selected.lon,
+                  }}
+                  onCloseClick={() => {
+                    setSelected(null);
+                  }}
+                >
+                  <div style={{ backgroundColor: "black" }}>
+                    <Heading as="h2" size="md">
+                      {selected.commonName}
+                    </Heading>
+                    <Text>{selected.id}</Text>
+                    <Text>{selected.additionalProperties.find(prop => prop.key === 'NbBikes').value} bike(s) available</Text>
+                    <Text>Last updated: {selected.additionalProperties.find(prop => prop.key ==='NbBikes').modified}</Text>
+                  </div>
+                </InfoBox>
+              )}
+            </GoogleMap>
+          </Container>
+        )}
+        {showHeatmap && (
+          <Container>
+            <Heading as='h4' size='md'>Bike accident heat map (2019 data)</Heading>
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              zoom={10}
+              center={center}
+              options={options}
+            >
+              <HeatmapLayer 
+                data={pedalCycleAccidentCoords}
+              />
+            </GoogleMap>
+          </Container>
+        )}
       </Container>
     </ChakraProvider>
   );
